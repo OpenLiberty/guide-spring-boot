231f128535ff23961190abc0b4a8e7e17db872fd
Gilbert Kwan <gkwan@ca.ibm.com>
2020-09-30 18:05:06 -0400

    Update travisTest.sh

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -26,8 +26,8 @@ else
   exit 1
 fi
 
-docker exec springBootContainer -- cat /logs/messages.log | grep product
-docker exec springBootContainer -- cat /logs/messages.log | grep java
+docker exec springBootContainer cat /logs/messages.log | grep product
+docker exec springBootContainer cat /logs/messages.log | grep java
 
 docker stop springBootContainer
 docker rm springBootContainer
