46e95d502de63a9c249ef46f9e76fc34c2934b05
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2020-12-03 14:13:04 -0500

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -11,7 +11,7 @@ done
 sed -i "\#<version>3.2<\/version>#,\#<configuration>#c<version>3.2</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DEVDATE"/"$DEVBUILD"</runtimeUrl></install>" pom.xml
 cat pom.xml
 
-sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:'$BUILDLEVEL';g" Dockerfile
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:'$BUILD';g" Dockerfile
 cat Dockerfile
 
 ../scripts/testApp.sh
