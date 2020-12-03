90e021501ee11128e4fe7c5baf4ecb3e6cf3933e
Austin Seto <austin.seto@ibm.com>
2020-12-03 14:29:43 -0500

    Fix sed commands

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -8,10 +8,10 @@ while getopts t:d:b:u: flag; do
     esac
 done
 
-sed -i "\#<version>3.2<\/version>#,\#<configuration>#c<version>3.2</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DEVDATE"/"$DEVBUILD"</runtimeUrl></install>" pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<artifactId>liberty-maven-plugin</artifactId><version>3.2.3</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DEVDATE"/"$DEVBUILD"</runtimeUrl></install>" pom.xml
 cat pom.xml
 
-sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:'$BUILD';g" Dockerfile
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" Dockerfile
 cat Dockerfile
 
 ../scripts/testApp.sh
