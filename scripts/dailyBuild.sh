f6e49260684355a51452b8c0266188c6985d4e5f
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2020-12-14 16:23:37 -0500

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -20,7 +20,7 @@ cat Dockerfile
 
 echo "Testing daily Docker image"
 
-sed -i "s;FROM open-liberty;FROM openliberty/daily:latest;g" Dockerfile
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM openliberty/daily:latest;g" Dockerfile
 cat Dockerfile
 
 ../scripts/testApp.sh
