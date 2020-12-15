fdf6f1c20f88fc1bd738ff59544b3c036016f04d
Austin Seto <austin.seto@ibm.com>
2020-12-15 16:10:12 -0500

    Verify build level of docker image

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -16,11 +16,18 @@ cat pom.xml
 sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" Dockerfile
 cat Dockerfile
 
-../scripts/testApp.sh
+sudo ../scripts/testApp.sh
 
 echo "Testing daily Docker image"
 
 sed -i "s;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";FROM openliberty/daily:latest;g" Dockerfile
 cat Dockerfile
 
-../scripts/testApp.sh
+IMAGEBUILDLEVEL=$(docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" openliberty/daily:latest)
+
+if [ $IMAGEBUILDLEVEL == $BUILD ] 
+then
+    sudo ../scripts/testApp.sh
+else
+    echo "Image does not match input build level for testing"
+fi
