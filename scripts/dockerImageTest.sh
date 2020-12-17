3f60a10c4959297a11a2f5d12d0263bd4efaa69a
Austin Seto <austin.seto@ibm.com>
2020-12-17 13:48:14 -0500

    Move docker image tests

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-while getopts t:d:b:u: flag; do
+while getopts t:d: flag; do
     case "${flag}" in
     t) DATE="${OPTARG}" ;;
     d) DRIVER="${OPTARG}" ;;
-    b) BUILD="${OPTARG}" ;;
-    u) DOCKER_USERNAME="${OPTARG}" ;;
     esac
 done
 
-echo "Testing daily build image"
+echo "Testing latest OpenLiberty Docker image"
 
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<artifactId>liberty-maven-plugin</artifactId><version>3.2.3</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install>" pom.xml
 cat pom.xml
 
-sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" Dockerfile
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM openliberty/daily:latest;g" Dockerfile
 cat Dockerfile
+
+docker pull "openliberty/daily:latest"
 
 sudo ../scripts/testApp.sh
