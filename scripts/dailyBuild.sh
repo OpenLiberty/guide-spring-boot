da603359579078e6c6d0bde5ca9788b26a7ac351
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-18 15:12:15 -0400

    update versions

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -5,15 +5,16 @@ while getopts t:d:b:u: flag; do
     d) DRIVER="${OPTARG}" ;;
     b) BUILD="${OPTARG}" ;;
     u) DOCKER_USERNAME="${OPTARG}" ;;
+    *) echo "Invalid option";;
     esac
 done
 
 echo "Testing daily build image"
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<artifactId>liberty-maven-plugin</artifactId><version>3.2.3</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install>" pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<artifactId>liberty-maven-plugin</artifactId><version>3.3.4</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install>" pom.xml
 cat pom.xml
 
-sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" Dockerfile
+sed -i "s;FROM openliberty/open-liberty:full-java8-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" Dockerfile
 cat Dockerfile
 
 sudo ../scripts/testApp.sh
