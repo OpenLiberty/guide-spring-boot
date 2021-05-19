f4bea344aa45d384f99d89e4414fc79dfaebe2b2
Priyanshi Patel <priyanshi.patel@ibm.com>
2021-05-19 09:31:02 -0400

    Fixing linter issues

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -9,7 +9,7 @@ done
 
 echo "Testing latest OpenLiberty Docker image"
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<artifactId>liberty-maven-plugin</artifactId><version>3.2.3</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install>" pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<artifactId>liberty-maven-plugin</artifactId><version>3.2.3</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/""$DATE""/""$DRIVER""</runtimeUrl></install>" pom.xml
 cat pom.xml
 
 sed -i "s;FROM openliberty/open-liberty:full-java11-openj9-ubi;FROM openliberty/daily:latest;g" Dockerfile
