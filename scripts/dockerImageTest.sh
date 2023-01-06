217a0c5f97b1e2ae9df91e1aefd8aa435fc7b9d4
Jakub Pomykala <jakub.pomykala@hotmail.co.uk>
2023-01-06 14:51:44 +0000

    Adding -ntp to mvnw in testApp.sh and updating all of maven wrapper to newest version

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -15,6 +15,6 @@ cat pom.xml
 sed -i "s;FROM icr.io/appcafe/open-liberty:full-java11-openj9-ubi;FROM openliberty/daily:latest;g" Dockerfile
 cat Dockerfile
 
-docker pull "openliberty/daily:latest"
+docker pull -q "openliberty/daily:latest"
 
 sudo ../scripts/testApp.sh
