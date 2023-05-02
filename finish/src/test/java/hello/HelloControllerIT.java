279be85df27ae99c9b6f60dc743006d7b8d0bb1c
Gilbert Kwan <gkwan@ca.ibm.com>
2023-05-02 15:24:11 -0400

    update version

--- finish/src/test/java/hello/HelloControllerIT.java
+++ finish/src/test/java/hello/HelloControllerIT.java
@@ -1,4 +1,4 @@
-package com.example.springboot;
+package hello;
 
 import org.junit.jupiter.api.Test;
 
--- finish/src/test/java/hello/HelloControllerTest.java
+++ finish/src/test/java/hello/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.springboot;
+package hello;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
