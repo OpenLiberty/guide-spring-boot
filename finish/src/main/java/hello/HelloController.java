2251d26b6d77a6883aebf8a03e3f666eb0d59437
Gilbert Kwan <gkwan@ca.ibm.com>
2023-05-02 11:42:01 -0400

    Update HelloController.java

--- finish/src/main/java/hello/HelloController.java
+++ finish/src/main/java/hello/HelloController.java
@@ -1,12 +1,12 @@
 package hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
     
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Greetings from Spring Boot!";
     }
