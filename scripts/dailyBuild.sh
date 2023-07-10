88fa96e35d2575863d1f7d1a876f3861a5189385
Gilbert Kwan <gkwan@ca.ibm.com>
2023-07-10 14:41:16 -0400

    Update dailyBuild.sh
    full image does not support features.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -15,6 +15,7 @@ sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<art
 cat pom.xml
 
 sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java8-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" Dockerfile
+sed -i "s;RUN features.sh;#RUN features.sh;g" Dockerfile
 cat Dockerfile
 
 sudo ../scripts/testApp.sh
