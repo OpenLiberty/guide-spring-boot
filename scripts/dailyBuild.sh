d024cd2884ea2b0b57cb715f9a25d60827bc0a3c
Gilbert Kwan <gkwan@ca.ibm.com>
2023-10-04 14:04:14 -0400

    update daily build test scripts

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -14,6 +14,7 @@ echo "Testing daily build image"
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<artifactId>liberty-maven-plugin</artifactId><version>3.8.2</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install>" pom.xml
 cat pom.xml
 
+sed -i "s;FROM icr.io/appcafe/open-liberty:full-java17-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" Dockerfile
 sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java17-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" Dockerfile
 sed -i "s;RUN features.sh;#RUN features.sh;g" Dockerfile
 cat Dockerfile
--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -12,6 +12,7 @@ echo "Testing latest OpenLiberty Docker image"
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<artifactId>liberty-maven-plugin</artifactId><version>3.8.2</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/""$DATE""/""$DRIVER""</runtimeUrl></install>" pom.xml
 cat pom.xml
 
+sed -i "s;FROM icr.io/appcafe/open-liberty:full-java17-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" Dockerfile
 sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java17-openj9-ubi;FROM openliberty/daily:latest;g" Dockerfile
 sed -i "s;RUN features.sh;#RUN features.sh;g" Dockerfile
 cat Dockerfile
