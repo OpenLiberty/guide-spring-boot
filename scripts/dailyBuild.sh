d600235e47426518ac9f4a7a2fe5cdd7541de9ae
Gilbert Kwan <gkwan@ca.ibm.com>
2023-11-15 14:44:22 -0500

    Update the guide to use Springboot 3.1.3 (#136)
    * Update to use Spring Boot 3.1.3 and springBoot-3.0 feature
    
    * Update workflow to use Java 17 for tests
    
    * use Java 17
    
    * update the spring-boot app
    
    * update the spring-boot app
    
    * update the output
    
    * update the output
    
    * update the output
    
    * check Java version
    
    * enable Java 17
    
    * enable Java 17
    
    * enable Java 17
    
    * enable Java 17
    
    * enable Java 17
    
    * update daily build test scripts
    
    * update daily build test scripts
    
    * update daily build test scripts
    
    * run docker logs if test fails
    
    * Update testApp.sh
    
    * Update testApp.sh
    
    * Update dailyBuild.sh
    
    * Update pom.xml
    
    * Update dailyBuild.sh
    
    * Update dailyBuild.sh
    
    * Update README.adoc
    
    ---------
    
    Co-authored-by: Thomas Watson <tjwatson@us.ibm.com>

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-while getopts t:d:b:u: flag; do
+while getopts t:d:b:u:j: flag; do
     case "${flag}" in
     t) DATE="${OPTARG}" ;;
     d) DRIVER="${OPTARG}" ;;
     b) BUILD="${OPTARG}" ;;
     u) DOCKER_USERNAME="${OPTARG}" ;;
+    j) JDK_LEVEL="${OPTARG}" ;;
     *) echo "Invalid option";;
     esac
 done
 
 echo "Testing daily build image"
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<artifactId>liberty-maven-plugin</artifactId><version>3.5.1</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install>" pom.xml
+if [ "$JDK_LEVEL" == "11" ]; then
+    echo "Test skipped because the guide does not support Java 11."
+    exit 0
+fi
+
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<artifactId>liberty-maven-plugin</artifactId><version>3.9</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install>" pom.xml
 cat pom.xml
 
-sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java8-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" Dockerfile
+sed -i "s;FROM icr.io/appcafe/open-liberty:full-java17-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD-java17;g" Dockerfile
+sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java17-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD-java17;g" Dockerfile
 sed -i "s;RUN features.sh;#RUN features.sh;g" Dockerfile
 cat Dockerfile
 
-sudo ../scripts/testApp.sh
+sudo -E ../scripts/testApp.sh
