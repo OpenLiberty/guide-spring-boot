c81691f0532a5bc5ad5faa1b6d7571be044c1682
Gilbert Kwan <gkwan@ca.ibm.com>
2024-05-08 10:55:48 -0400

    Revert "Remove docker build (#160)"
    This reverts commit 878db1ba7ea6dfd36f3f9b1bcc76eda6797d6614.

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -3,6 +3,8 @@ while getopts t:d:b:u:j: flag; do
     case "${flag}" in
     t) DATE="${OPTARG}" ;;
     d) DRIVER="${OPTARG}" ;;
+    b) BUILD="${OPTARG}" ;;
+    u) DOCKER_USERNAME="${OPTARG}" ;;
     j) JDK_LEVEL="${OPTARG}" ;;
     *) echo "Invalid option";;
     esac
@@ -17,5 +19,10 @@ fi
 
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<artifactId>liberty-maven-plugin</artifactId><version>3.10</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install>" pom.xml
 cat pom.xml
+
+sed -i "s;FROM icr.io/appcafe/open-liberty:full-java17-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD-java17;g" Dockerfile
+sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java17-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD-java17;g" Dockerfile
+sed -i "s;RUN features.sh;#RUN features.sh;g" Dockerfile
+cat Dockerfile
 
 sudo -E ../scripts/testApp.sh
