68731ba831169f39001591d372af11efa196a59c
Gilbert Kwan <gkwan@ca.ibm.com>
2025-01-14 16:50:31 -0500

    Update dailyBuild.sh (#213)

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -17,6 +17,11 @@ if [ "$JDK_LEVEL" == "11" ]; then
     exit 0
 fi
 
+if [ "$JDK_LEVEL" == "17" ]; then
+    echo "Test skipped because the guide does not support Java 17."
+    exit 0
+fi
+
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<artifactId>liberty-maven-plugin</artifactId><version>3.10</version><configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install>" pom.xml
 cat pom.xml
 
