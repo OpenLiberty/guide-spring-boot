238b51e3db1b72a5ad799ab9fbc2854f6dd905b2
Gilbert Kwan <gkwan@ca.ibm.com>
2025-09-30 11:55:57 -0400

    Change to use image from cp.stg.icr.io/cp/olc (#256)

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -14,22 +14,22 @@ sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#,\#<configuration>#c<art
 cat pom.xml
 
 if [[ "$OL_LEVEL" != "" ]]; then
-  sed -i "s;FROM icr.io/appcafe/open-liberty:full-java21-openj9-ubi-minimal;FROM cp.stg.icr.io/cp/olc/sps/open-liberty-vnext:$OL_LEVEL-full-java21-openj9-ubi-minimal;g" Dockerfile
-  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java21-openj9-ubi-minimal;FROM cp.stg.icr.io/cp/olc/sps/open-liberty-vnext:$OL_LEVEL-full-java21-openj9-ubi-minimal;g" Dockerfile
+  sed -i "s;FROM icr.io/appcafe/open-liberty:full-java21-openj9-ubi-minimal;FROM cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java21-openj9-ubi-minimal;g" Dockerfile
+  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java21-openj9-ubi-minimal;FROM cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java21-openj9-ubi-minimal;g" Dockerfile
 else
-  sed -i "s;FROM icr.io/appcafe/open-liberty:full-java21-openj9-ubi-minimal;FROM cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java21-openj9-ubi-minimal;g" Dockerfile
-  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java21-openj9-ubi-minimal;FROM cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java21-openj9-ubi-minimal;g" Dockerfile
+  sed -i "s;FROM icr.io/appcafe/open-liberty:full-java21-openj9-ubi-minimal;FROM cp.stg.icr.io/cp/olc/open-liberty-daily:full-java21-openj9-ubi-minimal;g" Dockerfile
+  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java21-openj9-ubi-minimal;FROM cp.stg.icr.io/cp/olc/open-liberty-daily:full-java21-openj9-ubi-minimal;g" Dockerfile
 fi
 sed -i "s;RUN features.sh;#RUN features.sh;g" Dockerfile
 cat Dockerfile
 
 echo "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin cp.stg.icr.io
 if [[ "$OL_LEVEL" != "" ]]; then
-  sudo docker pull -q "cp.stg.icr.io/cp/olc/sps/open-liberty-vnext:$OL_LEVEL-full-java21-openj9-ubi-minimal"
-  sudo echo "build level:"; docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" cp.stg.icr.io/cp/olc/sps/open-liberty-vnext:$OL_LEVEL-full-java21-openj9-ubi-minimal
+  sudo docker pull -q "cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java21-openj9-ubi-minimal"
+  sudo echo "build level:"; docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java21-openj9-ubi-minimal
 else
-  sudo docker pull -q "cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java21-openj9-ubi-minimal"
-  sudo echo "build level:"; docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java21-openj9-ubi-minimal
+  sudo docker pull -q "cp.stg.icr.io/cp/olc/open-liberty-daily:full-java21-openj9-ubi-minimal"
+  sudo echo "build level:"; docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" cp.stg.icr.io/cp/olc/open-liberty-daily:full-java21-openj9-ubi-minimal
 fi
 
 sudo -E ../scripts/testApp.sh
