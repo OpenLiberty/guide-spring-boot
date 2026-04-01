3b63952aa5494b210a0c388de89ebe1bf89fb594
Siaa Gor <siaa.gor@ibm.com>
2026-04-01 14:21:28 -0400

    investigating Github action failure case
    Signed-off-by: Siaa Gor <siaa.gor@ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -38,8 +38,9 @@ docker exec springBootContainer cat /logs/messages.log | grep java
 docker stop springBootContainer
 
 uname -r
+criu check
 cp ../instantOn/Dockerfile Dockerfile
-docker run --name springBootCheckpointContainer --privileged --env WLP_CHECKPOINT=afterAppStart springboot
+docker run --name springBootCheckpointContainer --privileged --cap-add=CHECKPOINT_RESTORE --cap-add=SYS_PTRACE --cap-add=SETPCAP -e XDG_RUNTIME_DIR=/tmp --env WLP_CHECKPOINT=afterAppStart springboot
 docker commit springBootCheckpointContainer springboot-instanton
 docker rm springBootCheckpointContainer
 docker images
