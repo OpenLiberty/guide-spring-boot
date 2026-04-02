2d76dcf347f638ee1f71521dce336fea90bbdf3a
Siaa Gor <siaa.gor@ibm.com>
2026-04-01 23:25:55 -0400

    changes
    Signed-off-by: Siaa Gor <siaa.gor@ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -49,25 +49,19 @@ capsh --print
 grep CapEff /proc/1/status
 cp ../instantOn/Dockerfile Dockerfile
 docker run -d --name springBootCheckpointContainer \
-  -v "$GITHUB_WORKSPACE/checkpoint-logs:/liberty/logs/checkpoint" \
   --privileged \
   --security-opt seccomp=unconfined \
+  --security-opt apparmor=unconfined \
+  --userns=host \
+  --network=host \
   --cap-add=CHECKPOINT_RESTORE \
   --cap-add=SYS_PTRACE \
   --cap-add=SETPCAP \
   -e XDG_RUNTIME_DIR=/tmp \
   -e WLP_CHECKPOINT=afterAppStart \
+  -v "$GITHUB_WORKSPACE/checkpoint-logs:/liberty/logs/checkpoint" \
   springboot && docker inspect springBootCheckpointContainer
 
-for i in $(seq 1 60):
-  if [ -f "$GITHUB_WORKSPACE/checkpoint-logs/checkpoint.log" ]; then
-    echo "checkpoint.log found"
-    cat "$GITHUB_WORKSPACE/checkpoint-logs/checkpoint.log"
-    break
-  else
-    echo "checkpoint.log not found, waiting..."
-    sleep 1
-  fi
 docker ps -a
 sleep 30
 docker logs springBootCheckpointContainer
