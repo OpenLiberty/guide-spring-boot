e1b0b69cc64f30eac4469acdf32bbe9410b8c5c9
Siaa Gor <siaa.gor@ibm.com>
2026-04-02 13:14:46 -0400

    changes
    Signed-off-by: Siaa Gor <siaa.gor@ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -49,7 +49,7 @@ capsh --print
 grep CapEff /proc/1/status
 cp ../instantOn/Dockerfile Dockerfile
 cat Dockerfile
-docker run -d --name springBootCheckpointContainer \
+docker run --name springBootCheckpointContainer \
   --privileged \
   --security-opt seccomp=unconfined \
   --security-opt apparmor=unconfined \
@@ -59,11 +59,10 @@ docker run -d --name springBootCheckpointContainer \
   --cap-add=SYS_PTRACE \
   --cap-add=SETPCAP \
   -e XDG_RUNTIME_DIR=/tmp \
+  -e WLP_CHECKPOINT=afterAppStart \
   springboot
-#  -e WLP_CHECKPOINT=afterAppStart \
+
 docker ps -a
-sleep 50
-docker exec springBootCheckpointContainer /opt/ol/wlp/bin/checkpoint.sh afterAppStart 
 #docker exec springBootCheckpointContainer bash -C 'id; ls -ld /liberty/logs/checkpoint; touch /liberty/logs/checkpoint/testfile'
 docker logs springBootCheckpointContainer
 #cat "$GITHUB_WORKSPACE/checkpoint-logs/checkpoint.log"
