2d64fc281913152f1a6fd577be81ac13f12228ce
Siaa Gor <siaa.gor@ibm.com>
2026-04-03 00:09:47 -0400

    change
    Signed-off-by: Siaa Gor <siaa.gor@ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -14,7 +14,7 @@ set -euxo pipefail
       -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
       -q clean package
 
-docker pull -q icr.io/appcafe/open-liberty:kernel-slim-java17-openj9-ubi
+docker pull -q icr.io/appcafe/open-liberty:kernel-slim-java17-openjdk-ubi
 
 docker build -t springboot .
 docker run -d --name springBootContainer --rm -p 9080:9080 -p 9443:9443 springboot
@@ -67,15 +67,18 @@ sudo docker run --name springBootCheckpointContainer \
   --ipc=host \
   -e XDG_RUNTIME_DIR=/tmp \
   -e WLP_CHECKPOINT=afterAppStart \
+  -e JAVA_TOOL_OPTIONS="-XX:+UnlockDiagonsticVMOptions -XX:+DebugNonSafepoints" \
+  -e CRIU_LOG_LEVEL=4 \
   springboot
 
 docker ps -a
+docker exec springBootCheckpointContainer cat /proc/sys/kernel/yama/ptrace_scope
 #docker exec springBootCheckpointContainer bash -C 'id; ls -ld /liberty/logs/checkpoint; touch /liberty/logs/checkpoint/testfile'
 docker logs springBootCheckpointContainer
 #cat "$GITHUB_WORKSPACE/checkpoint-logs/checkpoint.log"
 
-# docker cp springBootCheckpointContainer:/liberty/logs/checkpoint/checkpoint.log .
-# cat checkpoint.log
+docker cp springBootCheckpointContainer:/liberty/logs/checkpoint/checkpoint.log .
+cat checkpoint.log
 docker commit springBootCheckpointContainer springboot-instanton
 docker stop springBootCheckpointContainer
 docker rm springBootCheckpointContainer
