66803fa86f03a8909bfa43068e374ddf9476b5ed
Siaa Gor <siaa.gor@ibm.com>
2026-04-08 10:33:38 -0400

    another way
    Signed-off-by: Siaa Gor <siaa.gor@ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -39,26 +39,8 @@ docker stop springBootContainer
 
 uname -r
 
-CRIU_SOCKET_COMPATIBILITY=$(python3 -c "
-import socket 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-  s.getsockopt(1,34)
-  print('ok')
-except OSError: 
-  print('fail')
-except Exception as e:
-  print('fail with exception: ' + str(e))
-finally:
-  s.close()")
-
-if [ "$CRIU_SOCKET_COMPATIBILITY" != "ok" ]; then
-  echo "CRIU socket compatibility check failed. Check if your kernel supports CRIU and that you have the necessary permissions."
-  exit 0
-fi
-
 cp ../instantOn/Dockerfile Dockerfile
-docker run --name springBootCheckpointContainer --privileged --env WLP_CHECKPOINT=afterAppStart springboot
+docker run --name springBootCheckpointContainer --privileged --env WLP_CHECKPOINT=afterAppStart springboot || exit 0
 docker commit springBootCheckpointContainer springboot-instanton
 docker rm springBootCheckpointContainer
 docker images
